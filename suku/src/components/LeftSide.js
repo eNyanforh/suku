485d6286667001e2129d35dc1fa1e33f0e6a25d9
enyanforh <nyanforhen@gmail.com>
2024-07-15 04:10:14 -0700

    LeftSide Component Completed

--- suku/src/components/LeftSide.js
+++ suku/src/components/LeftSide.js
@@ -33,6 +33,23 @@ const LeftSide =()=>{
                     </span>
                 </Item>
             </ArtCard>
+            <CommunityCard>
+                <a>
+                    <span>Groups</span>
+                </a>
+                <a>
+                    <span>
+                        Events
+                        <img src="/images/plus-icon.svg" alt=""/>
+                    </span>
+                </a>
+                <a>
+                    <span>Follow Hashtags</span>
+                </a>
+                <a>
+                    <span>Discover more</span>
+                </a>
+            </CommunityCard>
         </Container>
     )
 }
@@ -129,11 +146,50 @@ color:rgba(0, 0, 0, 1);
 }
 `;
 const Item = styled.a`
-
+border-color: rgba(0, 0, 0, 0.8);
+text-align:left;
+padding:12px;
+font-size:12px;
+display:block;
+span {
+display:flex;
+align-items:center;
+color: rgba(0, 0, 0, 1);
+}
+&:hover{
+background-color:rgba(0, 0, 0, 0.08)
+}
 `;
 
+const CommunityCard = styled(ArtCard)`
+padding: 8px 0 0;
+text-align:left;
+display:flex;
+flex-direction: column;
+a{
+color:black;
+padding: 4px 12px 4px 12px;
+font-size:12px;
 
+&:hover{
+color:#0a66c2;
+}
 
+span{
+display:flex;
+align-items:center;
+justify-content:space-between; 
+}
 
+&:last-child {
+color:rgba(0, 0, 0, 0.6);
+border-top:1px solid #d6cec2;
+padding:12px;
 
+&:hover{
+background-color:rgba(0, 0, 0, 0.08);
+}
+}
+}
+`
 export default LeftSide;
